ad5950ee2d5814eab96a427e2e49a648b1d8f7c1
codingpro3 <mafhahouamjmamaham@gmail.com>
2026-06-17 20:11:57 +0100

    Implement test_predictions function for message validation
    Added a test function to validate message predictions.

---
+++ test_model.py
@@ -0,0 +1,25 @@
+# Run this cell to test your function and model. Do not modify contents.
+def test_predictions():
+  test_messages = ["how are you doing today",
+                   "sale today! to stop texts call 98912460324",
+                   "i dont want to go. can we try it a different day? available sat",
+                   "our new mobile video service is live. just install on your phone to start watching.",
+                   "you have won £1000 cash! call to claim your prize.",
+                   "i'll bring it tomorrow. don't forget the milk.",
+                   "wow, is your arm alright. that happened to me one time too"
+                  ]
+
+  test_answers = ["ham", "spam", "ham", "spam", "spam", "ham", "ham"]
+  passed = True
+
+  for msg, ans in zip(test_messages, test_answers):
+    prediction = predict_message(msg)
+    if prediction[1] != ans:
+      passed = False
+
+  if passed:
+    print("You passed the challenge. Great job!")
+  else:
+    print("You haven't passed yet. Keep trying.")
+
+test_predictions()
